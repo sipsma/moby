2f159400ac9020bb7f53a20285d19edda85c92cc
Erik Sipsma <erik@sipsma.dev>
2021-12-07 18:10:12 +0000

    updates needed for diffop
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- builder/builder-next/worker/worker.go
+++ builder/builder-next/worker/worker.go
@@ -190,6 +190,8 @@ func (w *Worker) ResolveOp(v solver.Vertex, s frontend.FrontendLLBBridge, sm *se
 			return ops.NewBuildOp(v, op, s, w)
 		case *pb.Op_Merge:
 			return ops.NewMergeOp(v, op, w)
+		case *pb.Op_Diff:
+			return ops.NewDiffOp(v, op, w)
 		}
 	}
 	return nil, errors.Errorf("could not resolve %v", v)
